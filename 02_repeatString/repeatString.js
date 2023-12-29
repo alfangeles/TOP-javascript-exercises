7d8c51b1bc9898cc093a7ff2a3beb8e04f7e9e15
alfredangeles <alf.angeles@gmail.com>
2023-12-29 07:32:09 -0500

    Get Ex. 02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, numberOfTime) {
+    //Create a variable to hold the string you're going to return
+    let stringRepeated = "";
+    
+    //returns ERROR with negative numbers
+    if (numberOfTime < 0) {
+        return "ERROR";
+        
+    }
 
+    //create a loop that repeats the given number of times and add the given string to the result on each loop.
+    for (let i = 0; i < numberOfTime; i++) {
+        stringRepeated += stringToRepeat;
+        
+        
+    }
+
+    return stringRepeated; 
 };
 
 // Do not edit below this line
