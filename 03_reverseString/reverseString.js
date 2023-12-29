a603b6324ea6668fe0aa9f5d61e0282ddaeca938
alfredangeles <alf.angeles@gmail.com>
2023-12-29 07:58:09 -0500

    Get Ex. 03 done.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    let stringReversed = ""
 
+    for (let i = 0; i <= stringToReverse.length; i++) {
+        if (i == 0) {
+            continue
+        }
+        stringReversed += stringToReverse[stringToReverse.length - i]
+        
+    }
+
+    return stringReversed;
+
+   
 };
+
+console.log(reverseString("hellow"));
 
 // Do not edit below this line
 module.exports = reverseString;
